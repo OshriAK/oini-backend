0e35cecce6773a273f9fd262fa66cca149452e89
Oshri Cohen <oshriakaa@gmail.com>
2021-05-19 17:38:04 +0300

    change schema

--- models/productModel.js
+++ models/productModel.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     brand: { type: String, required: true },
     model: { type: String, required: true },
-    makat: { type: String, required: true },
+    makat: { type: String, required: true, unique: true },
     image: { type: String, required: true },
     category: { type: String, required: true },
     price: { type: Number, required: true },
