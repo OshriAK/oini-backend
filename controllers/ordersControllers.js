8fc954fe8a4b612761b8ad5e83e2a5ee3f6de196
Oshri Cohen <oshriakaa@gmail.com>
2021-06-11 22:22:02 +0300

    add controllers

--- controllers/ordersControllers.js
+++ controllers/ordersControllers.js
@@ -1,10 +1,10 @@
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
-// export const getAllOrders = expressAsyncHandler(async (req, res) => {
-//   const orders = await Order.find({ user: req.user._id });
-//   res.send(orders);
-// });
+export const getOrdersByUser = expressAsyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.send(orders);
+});
 
 export const addNewOrder = expressAsyncHandler(async (req, res) => {
   if (req.body.orderItems.length === 0) {
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { isAuth } from '../utils.js';
 
-import { addNewOrder, getOrderById } from '../controllers/ordersControllers.js';
+import {
+  addNewOrder,
+  getOrderById,
+  getOrdersByUser,
+} from '../controllers/ordersControllers.js';
 
 const orderRouter = express.Router();
 
-// orderRouter.get('/mine', isAuth, getAllOrders);
+orderRouter.get('/mine', isAuth, getOrdersByUser);
 
 orderRouter.post('/', isAuth, addNewOrder);
 
